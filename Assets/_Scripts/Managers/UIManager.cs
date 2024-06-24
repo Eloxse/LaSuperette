aa1db2a1ade7ec2fd85370aaac3a96d2212f8c21
Eloxse <122028433+Eloxse@users.noreply.github.com>
2024-06-24 17:55:51 +0200

    Time code
    - Detect input entry
    - Set up video selection and buttons
    - Import videos for tests
    - Import assets 2D
    - Reorganize and clean all scripts

---
+++ Assets/_Scripts/Managers/UIManager.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    #region Variables
+
+    [Header("Scenes Manager")]
+    [SerializeField] private float timeBeforeLoad = 0.5f;
+
+    [Header("Display Time")]
+    [SerializeField] private TMP_Text[] txt_Clocks;
+
+    [Header("Screen Saver")]
+    [SerializeField] private GameObject screenSaver;
+    [SerializeField] private GameObject unlockScreen;
+    [SerializeField] private GameObject desk;
+
+    //Singleton.
+    private SFXManager _sfxManager;
+
+    #endregion
+
+    #region Built-In Methods
+
+    private void Update()
+    {
+        DisplayTime();
+    }
+
+    private void Start()
+    {
+        //Singleton.
+        _sfxManager = SFXManager.Instance;
+    }
+
+    #endregion
+
+    #region Current Time
+
+    /**
+     * <summary>
+     * Display real time.
+     * </summary>
+     */
+    private void DisplayTime()
+    {
+        string currentTime = DateTime.Now.ToShortTimeString();
+        foreach (TMP_Text txt_Clock in txt_Clocks)
+        {
+            txt_Clock.text = currentTime;
+        }
+    }
+
+    #endregion
+
+    #region Screen Saver
+
+    /**
+     * <summary>
+     * Button: switch from screen saver to unlock screen.
+     * Coroutine alows time before executing.
+     * </summary>
+     */
+    public void ExitScreenSaver()
+    {
+        StartCoroutine(DelayExitScreenSaver());
+    }
+
+    private IEnumerator DelayExitScreenSaver()
+    {
+        yield return new WaitForSeconds(timeBeforeLoad);
+        screenSaver.SetActive(false);
+    }
+
+    /**
+     * <summary>
+     * Button: connection to the desk.
+     * </summary>
+     */
+    public void DeskConnection()
+    {
+        StartCoroutine(DelayDeskConnection());
+    }
+
+    private IEnumerator DelayDeskConnection()
+    {
+        _sfxManager.SfxWindowsLaunch.Play();
+        yield return new WaitForSeconds(timeBeforeLoad);
+
+        unlockScreen.SetActive(false);
+        desk.SetActive(true);
+    }
+
+    #endregion
+}
---
+++ Assets/_Scripts/MonitoringController.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class MonitoringController : MonoBehaviour
+{
+    #region Variables
+
+    [Header("Scenes Manager")]
+    [SerializeField] private float timeBeforeLoad = 0.5f;
+
+    [Header("Monitoring Software")]
+    [SerializeField] private GameObject monitoringSoftware;
+
+    [Header("Video Monitoring")]
+    [SerializeField] private List<GameObject> allVideos;
+
+    //Time Code.
+    private string _input;
+
+    //Singleton.
+    private static MonitoringController _instance;
+
+    #endregion
+
+    #region Properties
+
+    //Video Monitoring.
+    public List<GameObject> AllVideos => allVideos;
+
+    //Singleton.
+    public static MonitoringController Instance => _instance;
+
+    #endregion
+
+    #region Built-In Methods
+
+    private void Awake()
+    {
+        if (_instance) Destroy(this);
+        _instance = this;
+    }
+
+    #endregion
+
+    #region Software
+
+    /**
+     * <summary>
+     * Button: open video monitoring software.
+     * </summary>
+     */
+    public void OpenVideoMonitoring()
+    {
+        StartCoroutine(DelayOpenSoftware());
+    }
+
+    private IEnumerator DelayOpenSoftware()
+    {
+        yield return new WaitForSeconds(timeBeforeLoad);
+        monitoringSoftware.SetActive(true);
+    }
+
+    #endregion
+
+    #region Video Monitoring
+
+    /**
+     * <summary>
+     * Activates the specified video and deactivates all others.
+     * </summary>
+     */
+    private void PlayVideo(GameObject videoToPlay)
+    {
+        foreach (GameObject video in allVideos)
+        {
+            video.SetActive(video == videoToPlay);
+        }
+    }
+
+    public void PlayVidTechnicalArea() => PlayVideo(allVideos.Find(v => v.name == "VID_TechnicalArea"));
+    public void PlayVidStorage() => PlayVideo(allVideos.Find(v => v.name == "VID_Storage"));
+    public void PlayVidHousehold() => PlayVideo(allVideos.Find(v => v.name == "VID_Household"));
+    public void PlayVidLockroom() => PlayVideo(allVideos.Find(v => v.name == "VID_Lockroom"));
+    public void PlayVidSupermarket() => PlayVideo(allVideos.Find(v => v.name == "VID_Supermarket"));
+    public void PlayVidBar() => PlayVideo(allVideos.Find(v => v.name == "VID_Bar"));
+    public void PlayVidStock() => PlayVideo(allVideos.Find(v => v.name == "VID_Stock"));
+    public void PlayVidDesk() => PlayVideo(allVideos.Find(v => v.name == "VID_Desk"));
+    public void PlayVidFridge() => PlayVideo(allVideos.Find(v => v.name == "VID_Fridge"));
+
+    #endregion
+
+    #region Time Code
+
+    /**
+     * <summary>
+     * Read any changes on input field.
+     * </summary>
+     */
+    public void ReadStringInput(string inputEntry)
+    {
+        _input = inputEntry;
+    }
+
+    #endregion
+}
