48431d208010b022a30ebbd787a08e9d43adf66b
Eloxse <122028433+Eloxse@users.noreply.github.com>
2024-07-11 18:04:01 +0200

    Desk
    - Modification on encryption animation
    - Import Assets 2D
    - Employee names
    - Wordpass window
    - Close window password
    - Password input field
    - Set up password window

--- Assets/_Scripts/Scenes/Windows3.cs
+++ Assets/_Scripts/Scenes/Windows3.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Windows3 : MonoBehaviour
 {
     #region Variables
 
     [Header("Startup")]
+    [SerializeField] private GameObject encryption;
     [SerializeField] private GameObject startupBackground;
     [SerializeField] private GameObject startup, desk;
+
+    [Header("Wordpass Window")]
+    [SerializeField] private GameObject window;
+    [SerializeField] private Button btn_ExitDoor, btn_Ok;
 
     //Singleton.
     private static CursorManager _cursorManager;
@@ -28,23 +34,77 @@ public class Windows3 : MonoBehaviour
 
     /**
      * <summary>
+     * Resume animation encryption.
+     * </summary>
+     */
+    public void ContinueEncryption()
+    {
+        encryption.SetActive(true);
+    }
+
+    /**
+     * <summary>
      * Active startup background after animation encryption.
      * </summary>
      */
     public void ActiveStartupBackground()
     {
         startupBackground.SetActive(true);
+        encryption.SetActive(false);
     }
 
     #endregion
 
     #region Desk
 
+    /**
+     * <summary>
+     * Unable desk and disable startup.
+     * </summary>
+     */
     public void Desk()
     {
         desk.SetActive(true);
         _cursorManager.SetLoadingCursor();
         startup.SetActive(false);
+    }
+
+    /**
+     * <summary>
+     * Button: Active wordpass window.
+     * </summary>
+     */
+    public void WordpassWindow()
+    {
+        StartCoroutine(LoadingWindow());
+    }
+
+    private IEnumerator LoadingWindow()
+    {
+        _cursorManager.SetLoadingCursor();
+        btn_ExitDoor.interactable = false;
+        yield return new WaitForSeconds(2.5f);
+        window.SetActive(true);
+    }
+
+    /**
+     * <summary>
+     * Button: Closed window.
+     * </summary>
+     */
+    public void CloseWindow()
+    {
+        window.SetActive(false);
+        btn_ExitDoor.interactable = true;
+    }
+
+    public void OkButton()
+    {
+        _cursorManager .SetLoadingCursor();
+        btn_Ok.interactable = false;
+        //if true = porte
+        //if false = error pop up
+        //btn ok interactable trues
     }
 
     #endregion
